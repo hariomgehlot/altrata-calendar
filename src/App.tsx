c11f189561679c135b84a3348083d2991d4336d8
BinaryBender <hariomg530@gmail.com>
2025-11-14 11:45:49 +0530

    fix: netlify publish issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Calendar } from "./components/Calendar/Calendar";
 function App() {
   return (
     <div className="flex items-center justify-center">
-      <Calendar date={new Date("06/24/2000")}></Calendar>
+      <Calendar date={new Date()}></Calendar>
     </div>
   );
 }
